7113ba6c49144dd34d8824b39dcdc3e57faf536c
iam-rethin <rethin.code@gmail.com>
2023-03-03 15:49:33 +0530

    since video 9

---
+++ app/screens/AudioList.js
@@ -0,0 +1,113 @@
+import { Text, View ,StyleSheet, ScrollView, Dimensions} from 'react-native'
+import React, { Component } from 'react'
+import { AudioContext } from '../context/AudioProvider'
+import {RecyclerListView , LayoutProvider} from 'recyclerlistview'
+import AudioListItem from '../components/AudioListItem'
+import Screen from '../components/Screen'
+import OptionModal from '../components/OptionModel'
+import { Audio } from 'expo-av';
+
+
+
+
+export class AudioList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      optionModalVisible: false,
+      playbackObj: null,
+      soundObj: null,
+      currentAudio: {}
+    };
+
+    this.currentItem = {};
+  }
+    static contextType = AudioContext
+    layoutProvider = new LayoutProvider((i) => 'audio' , (type , dim)=> {
+      switch(type){
+        case 'audio':
+            dim.width = Dimensions.get('window').width;
+            dim.height=70
+            break;
+
+            default:
+                dim.width = 0;
+            dim.height=0;
+
+      }
+     
+    });
+
+    handleAudioPress = async audio => {
+      //playing audio for the first time
+      if (this.state.soundObj===null){
+        const playbackObj = new Audio.Sound()
+        const status = await playbackObj.loadAsync({uri: audio.uri}, {shouldPlay: true});
+        return this.setState({...this.state, currentAudio: audio, playbackObj: playbackObj, soundObj: status})
+      }
+
+      //pause audio
+      if (this.state.soundObj.isLoaded && this.state.soundObj.isPlaying) {
+        const status = await this.state.playbackObj.setStatusAsync({shouldPlay: false});
+        return this.setState({...this.state, soundObj: status})
+      }
+
+      //resume audio
+      if (this.state.soundObj.isLoaded && !this.state.soundObj.isPlaying && this.state.currentAudio.id === audio.id){
+        const status = await this.state.playbackObj.playAsync();
+        return this.setState({...this.state, soundObj: status});
+      }
+    };
+
+    rowRenderer = (type , item )=>{
+      return <AudioListItem 
+      title={item.filename} 
+      duration = {item.duration} 
+      onAudioPress = {()=>this.handleAudioPress(item)} 
+      onOptionPress={()=>{
+        this.currentItem = item
+        this.setState({...this.state , optionModalVisible:true})
+      }}/>
+    }
+  render() {
+      return (
+      <AudioContext.Consumer>
+        {({dataProvider}) =>{
+            
+            return (
+            <Screen style={{flex:1 , padding:10 , borderBottomColor:'lightgray',borderBottomWidth:0.5 ,}}>
+            <RecyclerListView 
+            dataProvider={dataProvider} layoutProvider={this.layoutProvider} rowRenderer={this.rowRenderer}/>
+            <OptionModal 
+            onPlayPress={()=>console.log('playing audio.....')}
+            onPlayListPress={()=>{console.log('Added to the playlist')}}
+            currentItem={this.currentItem} onClose={() =>{
+              this.setState({...this.state, optionModalVisible:false})
+            }} visible={this.state.optionModalVisible}/>
+            </Screen>
+            );
+        }}
+      </AudioContext.Consumer>
+)}}
+
+
+// const styles = StyleSheet.create({
+//     container:{
+//         flex:1,
+//         justifyContent:'center',
+//         alignItems:'center'
+//     },
+//     text:{
+//         fontSize:15,
+//         width:'100%',
+//         padding:15,
+//         borderBottomColor:'lightgray',
+//         borderBottomWidth:0.3,
+
+        
+
+//     }
+// })
+
+export default AudioList
